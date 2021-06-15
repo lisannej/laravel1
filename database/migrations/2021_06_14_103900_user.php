954056b77936573b6626e065ccf1bdce683379e3
Lisanne Järv <lisannejarv@gmail.com>
2021-06-15 14:56:16 +0300

    Initial commit

---
+++ database/migrations/2021_06_14_103900_user.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class User extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('User', function(Blueprint $table){
+            $table->increments('id');
+            $table->string('email');
+            $table->string('password');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->integer('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
